bf1b2ed0db3bb2a9079b63d7d7ebc3cbf747653d
Seif Eldin <seifeldinrefaat@gmail.com>
2021-08-25 01:06:07 +0200

    Create note item list

--- app/src/main/java/com/neouto/munazam/ui/MainActivity.kt
+++ app/src/main/java/com/neouto/munazam/ui/MainActivity.kt
@@ -2,6 +2,9 @@ package com.neouto.munazam.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.neouto.munazam.R
 import com.neouto.munazam.databinding.ActivityMainBinding
 
@@ -11,8 +14,25 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    // --> get nav nav controller
+    private val navController: NavController by lazy {
+        findNavController(binding.navHostFragment.id)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
     }
+
+
+    override fun onResume() {
+        super.onResume()
+        // show action bar
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
 }
