10b36d4906703cb68024fa5ac0f5a495eb6e4b15
Seif Eldin <seifeldinrefaat@gmail.com>
2021-08-25 06:20:19 +0200

    ADD method insert note in database

--- app/src/main/java/com/neouto/munazam/ui/CreateNoteFragment.kt
+++ app/src/main/java/com/neouto/munazam/ui/CreateNoteFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.neouto.munazam.R
+import com.neouto.munazam.data.model.Note
 import com.neouto.munazam.databinding.FragmentCreateNoteBinding
 
 
@@ -22,6 +24,52 @@ class CreateNoteFragment: BaseFragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        binding.buttonSaveNote.setOnClickListener {
+            saveNoteInDatabase()
+        }
+
+    }
+
+
+    private fun saveNoteInDatabase() {
+        if (inputCheck(getDataFromUI().title, getDataFromUI().description)) {
+            sharedViewModel.insertNote(getDataFromUI())
+
+            // --> Back
+            findNavController().navigate(R.id.action_createNoteFragment_to_notesListFragment)
+        }
+    }
+
+    private fun inputCheck(title: String, description: String): Boolean {
+        return when {
+
+            title.isEmpty() -> {
+                binding.TitleInputLayout.error = "Required"
+                false
+            }
+
+            description.isEmpty() -> {
+                binding.descriptionInputLayout.error = "Required"
+                false
+            }
+
+            else -> true
+        }
+    }
+
+    private fun getDataFromUI(): Note {
+        return Note(
+            0,
+            binding.inputTitle.text.toString().trim(),
+            binding.inputDescription.text.toString().trim(),
+            "",
+            "",
+            "",
+            3
+        )
+    }
 
 }
