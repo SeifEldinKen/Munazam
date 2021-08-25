77c22f676852d08775e0aa5084a4c08e9462b18c
Seif Eldin <seifeldinrefaat@gmail.com>
2021-08-25 05:53:24 +0200

    Creat note viewModel

---
+++ app/src/main/java/com/neouto/munazam/viewmodel/NoteViewModel.kt
@@ -0,0 +1,49 @@
+package com.neouto.munazam.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.neouto.munazam.data.model.Note
+import com.neouto.munazam.data.repository.NoteRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class NoteViewModel(private val repository: NoteRepository): ViewModel() {
+
+    // --> all note live data
+    val fetchAllNoteLiveData: LiveData<List<Note>> by lazy {
+        repository.fetchAllNotesLiveData
+    }
+
+    // --> insert note to database
+    fun insertNote(note: Note) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.insertNote(note)
+        }
+    }
+
+    // --> delete note form database
+    fun deleteNote(note: Note) {
+        repository.deleteNote(note)
+    }
+
+    // --> delete all database
+    fun deleteAllNotes() {
+        repository.deleteAllNotes()
+    }
+
+}
+
+class NoteViewModelFactory(private val repository: NoteRepository): ViewModelProvider.Factory {
+
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+
+        if (modelClass.isAssignableFrom(NoteViewModel::class.java)) {
+            return NoteViewModel(repository) as T
+        }
+
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+
+}
