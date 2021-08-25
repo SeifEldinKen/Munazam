a65b48722de95d2c93aa2b881a5307e2e0530930
Seif Eldin <seifeldinrefaat@gmail.com>
2021-08-25 05:45:36 +0200

    Creat note Dao

---
+++ app/src/main/java/com/neouto/munazam/data/dao/NoteDao.kt
@@ -0,0 +1,25 @@
+package com.neouto.munazam.data.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.neouto.munazam.data.model.Note
+
+@Dao
+interface NoteDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(note: Note)
+
+    @Query("SELECT * FROM notes_table ORDER BY id ASC")
+    fun fetchAllNotes(): LiveData<List<Note>>
+
+    @Update
+    fun update(note: Note)
+
+    @Delete
+    fun delete(note: Note)
+
+    @Query("DELETE FROM notes_table")
+    fun deleteAllNotes()
+
+}
