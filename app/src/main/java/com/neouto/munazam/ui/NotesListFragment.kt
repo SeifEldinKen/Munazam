bf33fc99f539841552a93383812f3c74908751d4
Seif Eldin <seifeldinrefaat@gmail.com>
2021-08-25 06:09:51 +0200

    setup recyclerView in NotesListFragment

--- app/src/main/java/com/neouto/munazam/ui/NotesListFragment.kt
+++ app/src/main/java/com/neouto/munazam/ui/NotesListFragment.kt
@@ -7,12 +7,16 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.neouto.munazam.R
+import com.neouto.munazam.adapter.NoteAdapter
+import com.neouto.munazam.data.model.Note
 import com.neouto.munazam.databinding.FragmentNotesListBinding
 
 
 class NotesListFragment: BaseFragment() {
 
     private lateinit var binding: FragmentNotesListBinding
+
+    private val noteAdapter: NoteAdapter by lazy { NoteAdapter() }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,10 +29,25 @@ class NotesListFragment: BaseFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        // --> Observe the live data in database
+        observeNoteInDatabase()
+
         binding.fabCreateNote.setOnClickListener {
             findNavController().navigate(R.id.action_notesListFragment_to_createNoteFragment)
         }
 
+    }
+
+    private fun observeNoteInDatabase() {
+        sharedViewModel.fetchAllNoteLiveData.observe(viewLifecycleOwner, { notesList ->
+            setupRecyclerView(notesList)
+        })
+    }
+
+
+    private fun setupRecyclerView(newNotesList: List<Note>) {
+        noteAdapter.setData(newNotesList)
+        binding.recyclerViewNotes.adapter = noteAdapter
     }
 
 }
