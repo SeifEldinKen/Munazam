034474ca294e32128c855dcb9c00aec65da3236e
Seif Eldin <seifeldinrefaat@gmail.com>
2021-08-25 05:41:17 +0200

    ADD create note UI

--- app/src/main/java/com/neouto/munazam/ui/NotesListFragment.kt
+++ app/src/main/java/com/neouto/munazam/ui/NotesListFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.neouto.munazam.R
 import com.neouto.munazam.databinding.FragmentNotesListBinding
 
 
@@ -18,6 +20,15 @@ class NotesListFragment: Fragment() {
     ): View {
         binding = FragmentNotesListBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.fabCreateNote.setOnClickListener {
+            findNavController().navigate(R.id.action_notesListFragment_to_createNoteFragment)
+        }
+
     }
 
 }
