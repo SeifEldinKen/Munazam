a9a5528aa059a4e6c4e03fc8267365e6c0df5041
Seif Eldin <seifeldinrefaat@gmail.com>
2021-08-30 06:16:33 +0200

    ADD method swipe to delete

--- app/src/main/java/com/neouto/munazam/ui/NotesListFragment.kt
+++ app/src/main/java/com/neouto/munazam/ui/NotesListFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.*
 import androidx.appcompat.widget.SearchView
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
 import com.neouto.munazam.R
 import com.neouto.munazam.adapter.NoteAdapter
 import com.neouto.munazam.data.model.Note
@@ -47,14 +49,19 @@ class NotesListFragment: BaseFragment(), NoteOnClickListener, SearchView.OnQuery
         })
 
         binding.recyclerViewNotes.adapter = noteAdapter
+
+        // --> Swipe to Delete
+        swipeToDelete(binding.recyclerViewNotes)
+
+
     }
 
-    // --> this method delete all note form database
+    // --> This Methods Show AlertDialog to Confirm Removal of All Items from Database
     private fun deleteAllNotes() {
         showDialog(
             requireContext(),
-            "Delete all notes",
-            "Are you sure you want to remove all notes"
+            "Delete Everything?",
+            "Are you sure you want to remove everything?"
         ) {
             sharedViewModel.deleteAllNotes()
 
@@ -63,6 +70,30 @@ class NotesListFragment: BaseFragment(), NoteOnClickListener, SearchView.OnQuery
                 "Successfully Deleted All Notes"
             )
         }
+    }
+
+    // --> This Method Swipe to Delete Single Note from Database
+    private fun swipeToDelete(recyclerView: RecyclerView) {
+
+        val swipeToDeleteCallback = object: SwipeToDelete() {
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val itemToDelete = noteAdapter.oldNotesList[viewHolder.adapterPosition]
+
+                sharedViewModel.deleteNote(itemToDelete)
+
+                showToast(
+                    requireContext(),
+                    "Successfully removed: '${itemToDelete.title}'"
+                )
+
+            }
+
+        }
+
+        val itemTouchHelper = ItemTouchHelper(swipeToDeleteCallback)
+        itemTouchHelper.attachToRecyclerView(recyclerView)
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
@@ -78,9 +109,7 @@ class NotesListFragment: BaseFragment(), NoteOnClickListener, SearchView.OnQuery
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when(item.itemId) {
 
-            R.id.menuDeleteAllNotes -> {
-                deleteAllNotes()
-            }
+            R.id.menuDeleteAllNotes -> { deleteAllNotes() }
 
         }
 
@@ -101,7 +130,7 @@ class NotesListFragment: BaseFragment(), NoteOnClickListener, SearchView.OnQuery
         return true
     }
 
-    // --> this method search for item in database
+    // --> This Method Search for Item in Database
     private fun searchInDatabase(query: String) {
         val searchQueue = "%$query%"
 
